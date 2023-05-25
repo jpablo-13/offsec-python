c4fe5bbaecd1d4a050e335533b732fce15670211
jpablo-13 <jpablo@ieee.org>
2023-05-25 14:05:07 -0300

    some data types fun

---
+++ Basic/boolTest.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+#this may be set from a user database or after authentication
+adminBool = False
+
+if(adminBool):
+    print("You are an admin!")
+else:
+    print("You are NOT an admin!")
